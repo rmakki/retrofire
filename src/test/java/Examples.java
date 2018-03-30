ae8abbbc31ee3ac3ec7d9d83d8bf612b1dfea066
Rania Makki <rmakki@gmail.com>
2018-03-30 15:21:20 -0400

    added POST example

--- src/test/java/Examples.java
+++ src/test/java/Examples.java
@@ -28,26 +28,39 @@ public class Examples {
             // PUT request passing path and an object
             // Object to be passed
             UserDetails user1 = new UserDetails("uid1",100,150,70, "I love travel and discovering new cultures");
-
-            // PUT
             fbSvc.put("userDetails/uid1",user1);
 
-            // PUT request passing a path and raw json data
+            // PUT request passing raw json data
             // let's add another user
             String rawjson = "{\"userUID\":\"uid2\",\"nb_followers\":\"4000\",\"nb_following\":\"1000\",\"nb_posts\":\"300\"}";
-
-            // PUT
             fbSvc.put("userDetails/uid2",rawjson);
 
             // PATCH request passing a MAP
-            // Let's add the bio information to the user we just added (uid2)
+            // Let's add bio information to the user we just added (uid2)
             LinkedHashMap<String, Object> datamap = new LinkedHashMap<String, Object>();
             datamap.put("bio", "Musician/Band");
-
-            // PATCH
             fbSvc.patch("userDetails/uid2",datamap);
 
+            // POST
+            // User uid1 posts a photo, let's save the info
+            datamap.clear();
+            datamap.put("description","Enjoying this beautiful sunset, where are you traveling next?");
+            datamap.put("imageLink","https://myclouddatastorage/image231");
+            FirebaseResponse response = fbSvc.post("userPosts/uid1",datamap);
 
+            /*
+            // Retrieve the firebase unique id generated with the POST so that we use it in the next example
+            if (response.isSuccess()) {
+                response.getBody().
+
+            }
+
+            // POST
+            // someone comments on the previous post, let's save the comment
+            datamap.clear();
+            datamap.put("comment","Amazing shot!");
+            fbSvc.post("userPosts/uid1/comments",datamap);
+            */
 
             //UserDetails user2 = new UserDetails("uid2",4000,1000,300, "Musician/Band");
             // LinkedHashMap<String, Object> datamap = new LinkedHashMap<String, Object>();
