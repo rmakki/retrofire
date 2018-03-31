1261f8513d0b452f646e6b083709eaea4ca861c1
Rania Makki <rmakki@gmail.com>
2018-03-31 04:20:53 -0400

    added another POST example showing how to retrieve the firebase unique id returned in the POST request, added POJO class representing the unique id returned(FirebaseUID)

---
+++ src/main/java/model/firebaseUID.java
@@ -0,0 +1,25 @@
+package model;
+
+/**
+ * Created by raniamakki on 3/31/18.
+ */
+
+// POJO used to retrieve the UID returned by firebase for POST requests
+// sample json return from firebase {"name":"-L8sG9m_KP3m9ttEmua4"}
+
+public class FirebaseUID {
+
+    private String name;
+
+    public FirebaseUID() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
